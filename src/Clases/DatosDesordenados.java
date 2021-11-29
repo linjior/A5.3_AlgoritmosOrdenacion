4bc20c34319b4439e70f1ed4674c3f6a7d99b39d
vectorMillan <94257669+vectorMillan@users.noreply.github.com>
2021-11-29 09:19:50 -0600

    Agregue una clase para agregar numeros aleatorios con un rando de numeros, Tambien agregue el algoritmo de ordenamiento Radix en la clase DatosDesordenados y un metodo para mostrar el resultado del ordenamiento.

--- src/Clases/DatosDesordenados.java
+++ src/Clases/DatosDesordenados.java
@@ -10,5 +10,90 @@ package Clases;
  * @author linji
  */
 public class DatosDesordenados {
-    
+
+    int i, j, temporal, pasadas;
+
+    public DatosDesordenados() {
+        this.i = 0;
+        this.j = 0;
+        this.temporal = 0;
+    }
+
+    //Metodo burbuja version 1
+    public void burbuja(int[] arreglo) {
+        pasadas = 0;
+        for (i = 0; i < arreglo.length; i++) {
+            for (j = i + 1; j < arreglo.length; j++) {
+                if (arreglo[i] > arreglo[j]) {
+                    temporal = arreglo[i];
+                    arreglo[i] = arreglo[j];
+                    arreglo[j] = temporal;
+                }
+                pasadas++;
+            }
+        }
+        System.out.println("Termino en " + pasadas + " Pasadas");
+    }
+
+    //Metodo burbuja version 2
+    public void burbija2(int[] arreglo) {
+        pasadas = 0;
+        for (i = 1; i < arreglo.length; i++) {
+            for (j = 0; j < arreglo.length - 1; j++) {
+                if (arreglo[j] > arreglo[j + 1]) {
+                    temporal = arreglo[j];
+                    arreglo[j] = arreglo[j + 1];
+                    arreglo[j + 1] = temporal;
+                }
+                pasadas++;
+            }
+        }
+        System.out.println("Termino en " + pasadas + " Pasadas");
+    }
+
+    //Metodo Radix
+    public String Radix(int[] arreglo) {
+        int x, i, j;
+
+        for (x = Integer.SIZE - 1; x >= 0; x--) {//menon menos o mas mas
+            int auxiliar[] = new int[arreglo.length];
+            j = 0;
+            for (i = 0; i < arreglo.length; i++) {
+                boolean mover = arreglo[i] << x >= 0;//Si x<=0 devuelves un true o in false para asignar a este arreglo en la posicion i que se llama mover
+                /*
+                 *if(x>y){
+                    mayor=x;
+                 }else{
+                    mayor=y;
+                 }
+                 Operador Ternario ?
+                 resultado=(condicion)? valor1:valor2
+                 mayor=(x>y)?x:y;
+                 */
+                if (x == 0 ? !mover : mover) {
+                    auxiliar[j] = arreglo[i];
+                    j++;
+                } else {
+                    arreglo[i - j] = arreglo[i];
+                }
+            }
+            for (i = j; i < auxiliar.length; i++) {
+                auxiliar[i] = arreglo[i - j];
+            }
+            arreglo = auxiliar;
+        }
+        System.out.println("El arreglo ordenado con Radix es: ");
+        return mostrarArreglo(arreglo);
+    }
+
+    public String mostrarArreglo(int[] arreglo) {
+        int k;
+        String cadena = "";
+        for (k = 0; k < arreglo.length; k++) {
+            System.out.print("[" + arreglo[k] + "]" + "\n");
+            cadena = cadena + (k + 1) + "----->" + "[" + arreglo[k] + "]" + "\n";
+        }
+        System.out.println("");
+        return cadena;
+    }
 }
--- src/Clases/GeneradorNumeros.java
+++ src/Clases/GeneradorNumeros.java
@@ -10,5 +10,54 @@ package Clases;
  * @author linji
  */
 public class GeneradorNumeros {
+    private int nNumeros , s;
+    private int [] arreglo;
+
+    public GeneradorNumeros(int n, int s) {
+        this.arreglo = new int[n]; 
+        this.nNumeros = n;
+        this.s = s;
+    }
+
+    public int getnNumeros() {
+        return nNumeros;
+    }
+
+    public void setnNumeros(int nNumeros) {
+        this.nNumeros = nNumeros;
+    }
+
+    public int getS() {
+        return s;
+    }
+
+    public void setS(int s) {
+        this.s = s;
+    }
+
+    public int[] getArreglo() {
+        return arreglo;
+    }
+
+    public void setArreglo(int[] arreglo) {
+        this.arreglo = arreglo;
+    }
     
+    public int[] generarNumeros(){
+        this.arreglo = new int[nNumeros];
+        for (int i=0; i<nNumeros; i++){
+            arreglo[i] = (int)(Math.random() * s) + 1;
+        }
+        return arreglo;
+    }
+    
+    public String listaDesordenada(){
+        this.arreglo = new int[nNumeros];
+        String cadena = "";
+        for (int i=0; i<nNumeros; i++){
+            arreglo[i] = (int)(Math.random() * s) + 1;
+            cadena = cadena + (i+1) + "--->"+ arreglo[i]+"\n";
+        }
+        return cadena;
+    }
 }
